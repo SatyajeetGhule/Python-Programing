fa18b05420df1bd943295fcbfe96beac8c161a37
SatyajeetGhule <134911172+SatyajeetGhule@users.noreply.github.com>
2025-06-03 17:08:40 +0530

    Add files via upload

---
+++ Mathematical_Programs.py
@@ -0,0 +1,10 @@
+num1 = float(input(print("Enter Your 1st Digit : \n")))
+num2 = float(input(print("Enter Your 2st Digit : \n")))
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+
+print("both number are addition is : ", addition)
+print("both number are subtraction is : ", subtraction)
+print("both number are multiplication is : ", multiplication)
